e7c10b0902a8c3218e9e80291500acc368390ff4
Dqthinh2908 <quangthinh13@gmail.com>
2022-12-01 23:21:03 +0700

    bo sung file

--- public/js/app.js
+++ public/js/app.js
@@ -55977,8 +55977,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\xampp\htdocs\Fashion_Shop\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\xampp\htdocs\Fashion_Shop\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! E:\laragon\www\fashion_star_shop\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! E:\laragon\www\fashion_star_shop\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
